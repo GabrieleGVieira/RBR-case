a0982f40adb5e76ddbec162168d7be0a316a44be
Gabriele Gonçalves Vieira <gabrielevieira011@gmail.com>
2024-05-17 20:42:29 -0300

    feat: creating filter for get all employees

--- backend/entities/errorEnum.js
+++ backend/entities/errorEnum.js
@@ -1,7 +1,9 @@
-export const errorEnum = {
+const errorEnum = {
   emptyField: "EMPTY_FIELD",
   invalidName: "INVALID_NAME",
   invalidJobTitle: "INVALID_JOB_TITLE",
   invalidDepartment: "INVALID_DEPARTMENT",
   unknowError: "UNKNOW_ERROR",
 };
+
+export default errorEnum;
